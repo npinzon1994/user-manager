a18ac665ffc9131370470d4f7814c89403d2a7df
Nikki <Nikki@center99.cfjh.net>
2022-10-28 11:54:57 -0400

    Added states to capture user data and connected it all together in NewUser.js. Next we need to add validation to the form inputs.

--- src/components/Users/NewUser.js
+++ src/components/Users/NewUser.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./NewUser.module.css";
 import Card from "../UI/Card.js";
 import Button from "../UI/Button";
 
 const NewUser = (props) => {
+  //need state to capture user input
+  const[enteredUsername, setEnteredUsername] = useState('');
+  const[enteredAge, setEnteredAge] = useState('');
+  
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  }
+  
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUsername}/>
 
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age" />
+        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge}/>
 
         <Button type="submit" onClick={addUserHandler}>Add User</Button>
+      
       </form>
     </Card>
   );
