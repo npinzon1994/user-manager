2386bd5aad7c35abb641994aa06f53723feb9a94
Nikki Pinzon <npinzon1994@gmail.com>
2022-11-02 12:34:49 -0400

    Added portals to Modal.js

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Modal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
 
-const Modal = (props) => {
-  //overlay goes first because the modal will sit on top
-    
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
 
-    return (
-    <div>
-    <div className={classes.backdrop} onClick={props.onClose}></div> 
+const ModalOverlay = (props) => {
+  return (
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -21,7 +21,26 @@ const Modal = (props) => {
         <Button onClick={props.onClose}>OK</Button>
       </footer>
     </Card>
-    </div>
+  );
+};
+
+const Modal = (props) => {
+  //overlay goes first because the modal will sit on top
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClose={props.onClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
